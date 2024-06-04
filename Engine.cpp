c96c845ebff1f4bcf61082f0509a154df67a91ef
B4BF00LN4MU <boy566@naver.com>
2024-06-04 17:53:12 +0900

    20240603

--- Engine.cpp
+++ Engine.cpp
@@ -1,6 +1,11 @@
-#include "Engine.h"
+#include <iostream>
+#include <fstream>
 #include <conio.h> //_getch() 쓸려고 쓰는 STL
+
+#include "Engine.h"
 #include "Actor.h"
+
+using namespace std;
 
 UEngine::UEngine()
 {
@@ -23,6 +28,33 @@ void UEngine::Run()
 		Input();
 		Tick();
 		Render();
+	}
+}
+
+void UEngine::loadlevel(string MapFilename)
+{
+	char Map[255];
+
+	ifstream InputFile;
+
+	InputFile.open(MapFilename);
+
+	int X = 0;
+	int Y = 0;
+	while (InputFile.getline(Map, 80))
+	{
+		for (int X = 0; X < strlen(Map); ++X)
+		{
+			if (Map[X] == 'p')
+			{
+				SpawnActor(new APlayer(X, Y));
+			}
+			else if (Map[X] == "*")
+			{
+				SpawnActor(new AWall(X, Y));
+			}
+		}
+		Y++
 	}
 }
 
--- Engine.h
+++ Engine.h
@@ -19,6 +19,8 @@ public:
 
 	bool IsRunning;
 	
+	void loadlevel(string MapFilename);
+
 	vector<AActor*> Actors;
 
 protected:
